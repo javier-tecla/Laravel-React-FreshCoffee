d93c329c65399cd61ae41cf572dfcb90bb06a13b
javierBorjas <cristman11@gmail.com>
2024-06-20 00:09:30 -0300

    first commit

---
+++ laravel-quiosco/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\PedidoController;
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Almacenar ordenes
+    Route::apiResource('/pedidos', PedidoController::class);
+
+    Route::apiResource('/categorias', CategoriaController::class);
+    Route::apiResource('/productos', ProductoController::class);
+});
+
+
+
+// Autenticacion
+Route::post('/registro', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
